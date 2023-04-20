407e2b4c04fc5a05a1d0a09a50f6d527100f1117
Jasiek Dyrduł <johnygaming2@gmail.com>
2023-04-20 23:34:16 +0200

    backend

--- szachy-online.Api/szachy-online.Api/Program.cs
+++ szachy-online.Api/szachy-online.Api/Program.cs
@@ -13,9 +13,22 @@ namespace szachy_online.Api
     {
         public static void Main(string[] args)
         {
+            var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy(name: MyAllowSpecificOrigins,
+                                  policy =>
+                                  {
+                                      policy.WithOrigins("http://localhost:7225", "http://localhost:3000")
+                                                    .AllowAnyHeader()
+                                                    .AllowAnyMethod();
+                                  });
+            });
 
             builder.Services.AddControllers();
 
@@ -67,6 +80,8 @@ namespace szachy_online.Api
             // Configure the HTTP request pipeline.
 
             app.UseHttpsRedirection();
+
+            app.UseCors(MyAllowSpecificOrigins);
 
             app.UseAuthentication();
 
