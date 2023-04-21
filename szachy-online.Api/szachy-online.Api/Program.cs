bcd37d91a5142eb20c138d4aa59ded9e8b220e70
Jasiek Dyrduł <johnygaming2@gmail.com>
2023-04-21 16:11:49 +0200

    sync

--- szachy-online.Api/szachy-online.Api/Program.cs
+++ szachy-online.Api/szachy-online.Api/Program.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
 using szachy_online.Api.Data;
@@ -19,56 +18,52 @@ namespace szachy_online.Api
 
             // Add services to the container.
 
-            builder.Services.AddCors(options =>
-            {
+            builder.Services.AddCors(options => {
                 options.AddPolicy(name: MyAllowSpecificOrigins,
-                                  policy =>
-                                  {
-                                      policy.WithOrigins("http://localhost:7225", "http://localhost:3000")
-                                                    .AllowAnyHeader()
-                                                    .AllowAnyMethod();
-                                  });
+                  policy => {
+                      policy.WithOrigins("http://localhost:7225", "http://localhost:3000")
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+                  });
             });
 
             builder.Services.AddControllers();
 
-            builder.Services.AddDbContext<DataContext>(options =>
-            {
+            builder.Services.AddDbContext<DataContext>(options => {
                 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
             });
 
             var tokenOptions = builder.Configuration
-                .GetSection(TokenOptions.CONFIG_NAME)
-                .Get<TokenOptions>();
+              .GetSection(TokenOptions.CONFIG_NAME)
+              .Get<TokenOptions>();
             builder.Services.Configure<TokenOptions>(
-                builder.Configuration.GetSection(TokenOptions.CONFIG_NAME)
+              builder.Configuration.GetSection(TokenOptions.CONFIG_NAME)
             );
 
             builder.Services
-                .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-                .AddJwtBearer(
-                    o =>
+              .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+              .AddJwtBearer(
+                o => {
+                    o.TokenValidationParameters = new TokenValidationParameters
                     {
-                        o.TokenValidationParameters = new TokenValidationParameters
-                        {
-                            ClockSkew = TimeSpan.FromMinutes(1),
-                            IgnoreTrailingSlashWhenValidatingAudience = true,
-                            IssuerSigningKey = new SymmetricSecurityKey(
-                                Encoding.ASCII.GetBytes(tokenOptions.SigningKey)
-                            ),
-                            ValidateIssuerSigningKey = tokenOptions.ValidateSigningKey,
-                            RequireExpirationTime = true,
-                            RequireAudience = true,
-                            RequireSignedTokens = true,
-                            ValidateAudience = true,
-                            ValidateIssuer = true,
-                            ValidateLifetime = true,
-                            ValidAudience = tokenOptions.Audience,
-                            ValidIssuer = tokenOptions.Issuer
-                        };
-                        o.SaveToken = true;
-                    }
-                );
+                        ClockSkew = TimeSpan.FromMinutes(1),
+                        IgnoreTrailingSlashWhenValidatingAudience = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(
+                    Encoding.ASCII.GetBytes(tokenOptions.SigningKey)
+                  ),
+                        ValidateIssuerSigningKey = tokenOptions.ValidateSigningKey,
+                        RequireExpirationTime = true,
+                        RequireAudience = true,
+                        RequireSignedTokens = true,
+                        ValidateAudience = true,
+                        ValidateIssuer = true,
+                        ValidateLifetime = true,
+                        ValidAudience = tokenOptions.Audience,
+                        ValidIssuer = tokenOptions.Issuer
+                    };
+                    o.SaveToken = true;
+                }
+              );
 
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
