9f5f2b258e51454d1fca844aac3d7b81093abda0
Makrol <karolgardian6@gmail.com>
2023-04-23 13:09:12 +0200

    FrontEnd: login and register

--- szachy-online/src/services/AccountService.js
+++ szachy-online/src/services/AccountService.js
@@ -1,11 +1,14 @@
 import React from "react";
 import axios from "axios";
+import { useState } from "react";
+import { HostName } from "../HostName";
 
-export function login(login,password){
+
+export function login(login,password,setIsLogged,setStyleText,setStyleInput){
     console.log("start login");
     console.log("login: "+login);
     console.log("password: "+password);
-    axios.post('https://localhost:7225/api/Account/login',
+    axios.post(HostName+'/api/Account/login',
         {
                 userName: login,
                 password: password
@@ -13,9 +16,36 @@ export function login(login,password){
         .then(response => {
             localStorage.accessToken = response.data.accessToken;
             localStorage.refreshToken = response.data.refreshToken;
+            setIsLogged(true);
         })
         .catch(error => {
-            console.log(error.data);
+            setStyleText("account-text-error");
+            setStyleInput("account-input-error");
         })
     console.log("stop login");
 }
+export function logout(setIsLogged){
+    localStorage.removeItem("accessToken");
+    localStorage.removeItem("refreshToken");
+    setIsLogged(false);
+}
+export function register(id,name,surname,email,login,password){
+    axios.post(HostName+'/api/Account',
+    {
+        id: id,
+        name: name,
+        surname: surname,
+        email: email,
+        login: login,
+        password: password,
+        dateCreated: new Date(),
+    })
+    .then(response => {
+        console.log("git");
+    })
+    .catch(error =>{
+        console.log(error.data);
+    })
+
+}
+ 
