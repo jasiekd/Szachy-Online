b848d9f943b1a6889f8956f0fced30cf8829e087
Makrol <karolgardian6@gmail.com>
2023-05-11 22:09:19 +0200

    Frontend: Moving file loading to FileComponent

--- szachy-online/src/components/FileComponent.js
+++ szachy-online/src/components/FileComponent.js
@@ -1,18 +1,40 @@
 import continueIcon from "../img/right-arrow.png";
 import chessHistory from "../img/chessHistory.png";
+import { useNavigate } from "react-router-dom";
 
 export default function FileComponent({getStyle,show,hide})
 {
+    const navigate = useNavigate()
+    function fileReader(file){    
+   
+        var reader = new FileReader();
+        
+        reader.onload = function(e) {
+            var content = reader.result;
+            navigate("/chessBoard",{state:{content}});
+
+        }
+        
+        reader.readAsText(file);    
+    }
     return(
+
         <div className='option' style={getStyle} onMouseOver={show}  onMouseOut={hide}>
-            <button className='nav-btn'>
-                <img className="btn-img" src={continueIcon} alt=""/>
-                <p className='slide-btn-text'>Wznów gre z pliku</p>
-            </button>
-            <button className='nav-btn'>
-                <img className="btn-img" src={chessHistory} alt=""/>
-                <p className='slide-btn-text'>Historia rozgrywki z pliku</p>
-            </button>
+        {/* <button  className='nav-btn' >
+            <img className="btn-img" src={continueIcon} alt=""/>
+            <p className='slide-btn-text'>Wznów gre z pliku</p>
+        </button> */}
+        <label htmlFor="fileInput"  className='nav-btn'>
+        <img className="btn-img" src={continueIcon} alt=""/>
+        <p className='slide-btn-text'>Wznów gre z pliku</p>
+        </label>
+        <input type="file"  id="fileInput" style={{display:'none'}} accept='.pgn' onChange={(event)=>fileReader(event.target.files[0])}/>
+
+        <button className='nav-btn'>
+            <img className="btn-img" src={chessHistory} alt=""/>
+            <p className='slide-btn-text'>Historia rozgrywki z pliku</p>
+        </button>
         </div>
+
     )
 }
