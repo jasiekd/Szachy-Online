94d19f08f94bec2107d8b31fcd9d49a570439e02
Jasiek Dyrduł <johnygaming2@gmail.com>
2023-06-10 18:40:41 +0200

    backend: cancel and timeout invitation

--- szachy-online.Api/szachy-online.Api/Hubs/InvitationHub.cs
+++ szachy-online.Api/szachy-online.Api/Hubs/InvitationHub.cs
@@ -6,9 +6,15 @@ namespace szachy_online.Api.Hubs
     {
         public async Task SendGameInvitation(string senderGuid,string receiverGuid, string color)
         {
-
             await Clients.All.SendAsync(receiverGuid, senderGuid, color);
-
+        }
+        public async Task CancelGameInvitation(string receiverGuid)
+        {
+            await Clients.All.SendAsync(receiverGuid, "Cancel Invitation");
+        }
+        public async Task TimeOutGameInvitation(string receiverGuid)
+        {
+            await Clients.All.SendAsync(receiverGuid, "Friend not responded");
         }
     }
 }
